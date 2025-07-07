e145165bfc8203595f8f596910c10c2129bcb817
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-07-07 12:39:46 +0530

    updated

---
+++ bankingapp/src/main/java/com/siemens/models/Address.java
@@ -0,0 +1,11 @@
+package com.siemens.models;
+
+public class Address {
+    private String doorNo;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String country;
+
+}
---
+++ bankingapp/src/main/java/com/siemens/models/Customer.java
@@ -0,0 +1,15 @@
+package com.siemens.models;
+
+public class Customer {
+
+    private long accountNUmber;
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String email;
+    private String contactNo;
+    private String password;
+
+
+
+}
