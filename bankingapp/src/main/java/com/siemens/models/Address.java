841ed8bc23d7e38b7b00e651abb412eb39acc146
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-07-07 12:52:28 +0530

    updated

--- bankingapp/src/main/java/com/siemens/models/Address.java
+++ bankingapp/src/main/java/com/siemens/models/Address.java
@@ -1,5 +1,12 @@
 package com.siemens.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Address {
     private String doorNo;
     private String street;
--- bankingapp/src/main/java/com/siemens/models/Customer.java
+++ bankingapp/src/main/java/com/siemens/models/Customer.java
@@ -1,7 +1,12 @@
 package com.siemens.models;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Customer {
-
     private long accountNUmber;
     private String firstName;
     private String lastName;
@@ -9,11 +14,5 @@ public class Customer {
     private String email;
     private String contactNo;
     private String password;
-
     private Address address;
-
-
-
-
-
 }
