1a04f209393c441e0ad5cc95999ed3277bb2f4cb
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-07-08 12:27:05 +0530

    updated

--- bankingapp/src/main/java/com/siemens/models/Corporate.java
+++ bankingapp/src/main/java/com/siemens/models/Corporate.java
@@ -1,12 +1,14 @@
 package com.siemens.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class Corporate extends Customer{
 
     private CompanyType companyType;
--- bankingapp/src/main/java/com/siemens/util/InheritanceDemo.java
+++ bankingapp/src/main/java/com/siemens/util/InheritanceDemo.java
@@ -1,6 +1,8 @@
 package com.siemens.util;
 
 import com.github.javafaker.Faker;
+import com.siemens.models.CompanyType;
+import com.siemens.models.Corporate;
 import com.siemens.models.Gender;
 import com.siemens.models.Individual;
 
@@ -23,10 +25,26 @@ public class InheritanceDemo {
                 .gender(generateGender()).build();
         System.out.println(individual);
 
+        Corporate corporate= Corporate
+                .builder()
+                .firstName(faker.name().firstName())
+                .lastName(faker.name().lastName())
+                .middleName(faker.name().username())
+                .email(faker.internet().emailAddress())
+                .contactNo(faker.phoneNumber().phoneNumber())
+                .password(faker.internet().password())
+                .companyType(generateCompanyType())
+                .build();
+        System.out.println(corporate);
+
     }
 
     public static Gender generateGender(){
         int length=Gender.values().length;
         return Gender.values()[length-1];
+    }
+    public static CompanyType generateCompanyType(){
+        int length=CompanyType.values().length;
+        return CompanyType.values()[length-1];
     }
 }
