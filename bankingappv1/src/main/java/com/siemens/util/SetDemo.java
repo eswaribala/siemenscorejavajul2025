f7883e9e3a9af1d23702fc522158b6b088e630dd
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-07-09 12:34:39 +0530

    updated

--- bankingappv1/src/main/java/com/siemens/util/SetDemo.java
+++ bankingappv1/src/main/java/com/siemens/util/SetDemo.java
@@ -9,15 +9,26 @@ public class SetDemo {
         Faker faker = new Faker();
         HashSet<Integer> set1 = new HashSet<>();
         //unique numbers
-        for(int i=0;i<100;i++){
-            set1.add(faker.random().nextInt(1000,1050));
+        for(int i=0;i<10;i++){
+            set1.add(faker.random().nextInt(1000,1005));
         }
         System.out.println(set1);
         HashSet<Integer> set2 = new HashSet<>();
         //unique numbers
-        for(int i=0;i<100;i++){
-            set2.add(faker.random().nextInt(1000,1050));
+        for(int i=0;i<10;i++){
+            set2.add(faker.random().nextInt(1003,1008));
         }
         System.out.println(set2);
+
+        //union
+        set1.addAll(set2);
+        System.out.println(set1);
+        //intersection
+        set1.retainAll(set2);
+        System.out.println(set1);
+        //minus
+        set1.removeAll(set2);
+        System.out.println(set1);
+
     }
 }
