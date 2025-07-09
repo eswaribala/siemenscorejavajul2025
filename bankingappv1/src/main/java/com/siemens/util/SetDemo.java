04b92cf8221d777208e1854ec03c4aeb373c0524
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-07-09 12:25:39 +0530

    updated

--- bankingappv1/src/main/java/com/siemens/util/SetDemo.java
+++ bankingappv1/src/main/java/com/siemens/util/SetDemo.java
@@ -7,10 +7,17 @@ import java.util.HashSet;
 public class SetDemo {
     public static void main(String[] args) {
         Faker faker = new Faker();
-        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> set1 = new HashSet<>();
+        //unique numbers
         for(int i=0;i<100;i++){
-            set.add(faker.random().nextInt(1,50));
+            set1.add(faker.random().nextInt(1000,1050));
         }
-        System.out.println(set);
+        System.out.println(set1);
+        HashSet<Integer> set2 = new HashSet<>();
+        //unique numbers
+        for(int i=0;i<100;i++){
+            set2.add(faker.random().nextInt(1000,1050));
+        }
+        System.out.println(set2);
     }
 }
